d24519f5c9249b3992b2f4f20e90b21d922493bf
Steven Githens <swgithen@mtu.edu>
2017-02-08 17:27:42 -0800

    django-1.8 Cleaning up test suite
    Signed-off-by: Steven Githens <swgithen@mtu.edu>

--- test_suite.py
+++ test_suite.py
@@ -12,8 +12,4 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
 
     from django.core.management import execute_from_command_line
-    from django.core.management import call_command
-
-    # execute_from_command_line(sys.argv)
     execute_from_command_line(['manage.py', 'test', 'tests'])
-    # call_command('test', *apps)
